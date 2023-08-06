215a4486054394d8f81c908e627ce340b028a115
Danuk <danuk77@live.co.uk>
2023-08-06 23:56:59 +0100

    Initialised firebase functions

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// Firebase configuration information
+const firebaseConfig = {
+    apiKey: "AIzaSyAomQixQ9gGMNCYeYoeo66kFWYWrQfEN2g",
+    authDomain: "idea-garden-a35f6.firebaseapp.com",
+    projectId: "idea-garden-a35f6",
+    storageBucket: "idea-garden-a35f6.appspot.com",
+    messagingSenderId: "615250599761",
+    appId: "1:615250599761:web:40bd4571838df549197d70",
+    measurementId: "G-3QF8JBH91H"
+};
+
+export default firebaseConfig;
