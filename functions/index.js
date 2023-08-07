a48cabd244e926d8115ae4ae215a1209c7cae321
Danuk <danuk77@live.co.uk>
2023-08-07 21:20:27 +0100

    Tested a cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,10 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const {onCall} = require("firebase-functions/v2/https");
+const {setGlobalOptions} = require("firebase-functions/v2");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Required to allow deployment
+setGlobalOptions({maxInstances: 10});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.testFirebase = onCall((request) => {
+  return `Hello world!`;
+});
